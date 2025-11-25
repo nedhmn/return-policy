0a584a095f30d0550c4c60a196335bf6a127ee22
nedhmn <hermannned@gmail.com>
2025-11-25 03:32:00 -0500

    feat: sitemap & robots

---
+++ app/sitemap.ts
@@ -0,0 +1,24 @@
+import type { MetadataRoute } from 'next'
+
+export default function sitemap(): MetadataRoute.Sitemap {
+  return [
+    {
+      url: 'https://www.returnpolicystays.com',
+      lastModified: new Date(),
+      changeFrequency: 'monthly',
+      priority: 1,
+    },
+    {
+      url: 'https://www.returnpolicystays.com/terms',
+      lastModified: new Date(),
+      changeFrequency: 'yearly',
+      priority: 0.5,
+    },
+    {
+      url: 'https://www.returnpolicystays.com/privacy',
+      lastModified: new Date(),
+      changeFrequency: 'yearly',
+      priority: 0.5,
+    },
+  ]
+}
